81be5fb49a397520b94d0d9d02415cdca9eb187f
ha7ilm <retzlerandras@gmail.com>
2016-08-11 22:57:46 +0200

    Now you can toggle mathbox and waterfall

--- htdocs/openwebrx.js
+++ htdocs/openwebrx.js
@@ -2101,9 +2101,10 @@ function mathbox_init()
 
 function mathbox_toggle()
 {
-	mathbox_container.style.display="block";
+
 	if(mathbox_mode == MATHBOX_MODES.UNINITIALIZED) mathbox_init();
 	mathbox_mode = (mathbox_mode == MATHBOX_MODES.NONE) ? MATHBOX_MODES.WATERFALL : MATHBOX_MODES.NONE;
+	mathbox_container.style.display = (mathbox_mode == MATHBOX_MODES.WATERFALL) ? "block" : "none";
 	mathbox_clear_data();
 	waterfall_clear();
 }
