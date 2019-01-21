28f84c518808b7cc68c35859ec378e0291ef2643
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-01-21 22:53:59 +0000

    pushes need to be separate

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
-docker push jketterl/openwebrx jketterl/openwebrx-rtlsdr jketterl/openwebrx-sdrplay
+docker push jketterl/openwebrx
+docker push jketterl/openwebrx-rtlsdr
+docker push jketterl/openwebrx-sdrplay
