7bec9eaa87c47eb982ce17c45d40b46bec534f5b
Jakob Ketterl <jakob.ketterl@suedeutsche.de>
2019-01-24 17:07:12 +0100

    don't build/push the latest tag, that's a manifest now

--- build.sh
+++ build.sh
@@ -17,9 +17,3 @@ docker build --build-arg BASE_IMAGE=$BASE_IMAGE -t openwebrx-base:$ARCH -f docke
 docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-rtlsdr:$ARCH -t jketterl/openwebrx:$ARCH -f docker/Dockerfiles/Dockerfile-rtlsdr .
 docker build --build-arg ARCH=$ARCH -t openwebrx-soapysdr-base:$ARCH -f docker/Dockerfiles/Dockerfile-soapysdr .
 docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-sdrplay:$ARCH -f docker/Dockerfiles/Dockerfile-sdrplay .
-
-if [ "$ARCH" == "armv7l" ]; then
-  for image in openwebrx openwebrx-rtlsdr openwebrx-sdrplay; do
-    docker tag jketterl/$image:$ARCH jketterl/$image:latest
-  done
-fi
--- push.sh
+++ push.sh
@@ -5,7 +5,4 @@ ARCH=$(uname -m)
 
 for image in openwebrx openwebrx-rtlsdr openwebrx-sdrplay; do
   docker push jketterl/$image:$ARCH
-  if [ "$ARCH" == "armv7l" ]; then
-    docker push jketterl/$image:latest
-  fi
 done
