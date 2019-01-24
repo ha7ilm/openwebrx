6294797466ca48c780302b5a578bb76519f506ea
Jakob Ketterl <jakob.ketterl@suedeutsche.de>
2019-01-24 17:24:15 +0100

    add hackrf support

--- build.sh
+++ build.sh
@@ -17,3 +17,4 @@ docker build --build-arg BASE_IMAGE=$BASE_IMAGE -t openwebrx-base:$ARCH -f docke
 docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-rtlsdr:$ARCH -t jketterl/openwebrx:$ARCH -f docker/Dockerfiles/Dockerfile-rtlsdr .
 docker build --build-arg ARCH=$ARCH -t openwebrx-soapysdr-base:$ARCH -f docker/Dockerfiles/Dockerfile-soapysdr .
 docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-sdrplay:$ARCH -f docker/Dockerfiles/Dockerfile-sdrplay .
+docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-hackrf:$ARCH -f docker/Dockerfiles/Dockerfile-hackrf .
--- push.sh
+++ push.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 
 ARCH=$(uname -m)
 
-for image in openwebrx openwebrx-rtlsdr openwebrx-sdrplay; do
+for image in openwebrx openwebrx-rtlsdr openwebrx-sdrplay openwebrx-hackrf; do
   docker push jketterl/$image:$ARCH
 done
