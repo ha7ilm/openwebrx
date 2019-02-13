7e17cf2544228cbf765ffd0628f72cdadf56ee97
Norman Gray <norman@astro.gla.ac.uk>
2019-02-13 19:42:47 +0000

    Replace server_hostname with server_url. Makes it easier to manage reverse proxying if we don't try to be clever about our URI.

--- htdocs/openwebrx.js
+++ htdocs/openwebrx.js
@@ -1692,11 +1692,12 @@ String.prototype.startswith=function(str){ return this.indexOf(str) == 0; }; //h
 
 function open_websocket()
 {
-	//if(ws_url.startswith("ws://localhost:")&&window.location.hostname!="127.0.0.1"&&window.location.hostname!="localhost")
-	//{
-		//divlog("Server administrator should set <em>server_hostname</em> correctly, because it is left as <em>\"localhost\"</em>. Now guessing hostname from page URL.",1);
-		ws_url="ws://"+(window.location.origin.split("://")[1])+"/ws/"; //guess automatically -> now default behaviour
-	//}
+	// Determine the home URL based on the current location
+ 	ws_url = "ws://" + window.location.host + window.location.pathname + "ws/";
+ 	// We would like something like the following, but the URL() constructor doesn't
+ 	// do any useful URL normalisation.
+ 	//ws_url = new URL("ws/", "ws://" + window.location.host + window.location.pathname);
+
 	if (!("WebSocket" in window))
 		divlog("Your browser does not support WebSocket, which is required for WebRX to run. Please upgrade to a HTML5 compatible browser.");
 	ws = new WebSocket(ws_url+client_id);
