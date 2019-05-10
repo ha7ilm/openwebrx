b3d5f924c3d5d154e240ffc75158270dc2ab5fda
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-05-10 14:28:29 +0200

    rewrite urls to work again

--- htdocs/openwebrx.js
+++ htdocs/openwebrx.js
@@ -136,14 +136,14 @@ function toggleMute()
 	if (mute) {
 		mute = false;
 		e("openwebrx-mute-on").id="openwebrx-mute-off";
-		e("openwebrx-mute-img").src="gfx/openwebrx-speaker.png";
+		e("openwebrx-mute-img").src="static/gfx/openwebrx-speaker.png";
 		e("openwebrx-panel-volume").disabled=false;
 		e("openwebrx-panel-volume").style.opacity=1.0;
 		e("openwebrx-panel-volume").value = volumeBeforeMute;
 	} else {
 		mute = true;
 		e("openwebrx-mute-off").id="openwebrx-mute-on";
-		e("openwebrx-mute-img").src="gfx/openwebrx-speaker-muted.png";
+		e("openwebrx-mute-img").src="static/gfx/openwebrx-speaker-muted.png";
 		e("openwebrx-panel-volume").disabled=true;
 		e("openwebrx-panel-volume").style.opacity=0.5;
 		volumeBeforeMute = e("openwebrx-panel-volume").value;
