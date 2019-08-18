e4ef364aa87aa0aaa2718200e0aaa67a11527136
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-08-18 22:03:41 +0000

    looks like we have some additional dependencies now

--- docker/scripts/install-dependencies.sh
+++ docker/scripts/install-dependencies.sh
@@ -14,8 +14,8 @@ function cmakebuild() {
 
 cd /tmp
 
-STATIC_PACKAGES="sox fftw python3 netcat-openbsd libsndfile lapack libusb qt5-qtbase qt5-qtmultimedia qt5-qtserialport"
-BUILD_PACKAGES="git libsndfile-dev fftw-dev cmake ca-certificates make gcc musl-dev g++ lapack-dev linux-headers autoconf automake libtool texinfo gfortran libusb-dev qt5-qtbase-dev qt5-qtmultimedia-dev qt5-qtserialport-dev asciidoctor asciidoc"
+STATIC_PACKAGES="sox fftw python3 netcat-openbsd libsndfile lapack libusb qt5-qtbase qt5-qtmultimedia qt5-qtserialport qt5-qttools"
+BUILD_PACKAGES="git libsndfile-dev fftw-dev cmake ca-certificates make gcc musl-dev g++ lapack-dev linux-headers autoconf automake libtool texinfo gfortran libusb-dev qt5-qtbase-dev qt5-qtmultimedia-dev qt5-qtserialport-dev qt5-qttools-dev asciidoctor asciidoc"
 
 apk add --no-cache $STATIC_PACKAGES
 apk add --no-cache --virtual .build-deps $BUILD_PACKAGES
@@ -79,7 +79,7 @@ cmakebuild digiham
 git clone https://github.com/f4exb/dsd.git
 cmakebuild dsd
 
-WSJT_DIR=wsjtx-2.0.1
+WSJT_DIR=wsjtx-2.1.0
 WSJT_TGZ=${WSJT_DIR}.tgz
 wget http://physics.princeton.edu/pulsar/k1jt/$WSJT_TGZ
 tar xvfz $WSJT_TGZ
