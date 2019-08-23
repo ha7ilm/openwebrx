62e9a395577e189d72f5864ba15d40c4357bfd54
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-08-23 22:21:30 +0200

    add direwolf to docker build

--- docker/scripts/install-dependencies.sh
+++ docker/scripts/install-dependencies.sh
@@ -14,8 +14,8 @@ function cmakebuild() {
 
 cd /tmp
 
-STATIC_PACKAGES="sox fftw python3 netcat-openbsd libsndfile lapack libusb qt5-qtbase qt5-qtmultimedia qt5-qtserialport qt5-qttools"
-BUILD_PACKAGES="git libsndfile-dev fftw-dev cmake ca-certificates make gcc musl-dev g++ lapack-dev linux-headers autoconf automake libtool texinfo gfortran libusb-dev qt5-qtbase-dev qt5-qtmultimedia-dev qt5-qtserialport-dev qt5-qttools-dev asciidoctor asciidoc"
+STATIC_PACKAGES="sox fftw python3 netcat-openbsd libsndfile lapack libusb qt5-qtbase qt5-qtmultimedia qt5-qtserialport qt5-qttools alsa-lib"
+BUILD_PACKAGES="git libsndfile-dev fftw-dev cmake ca-certificates make gcc musl-dev g++ lapack-dev linux-headers autoconf automake libtool texinfo gfortran libusb-dev qt5-qtbase-dev qt5-qtmultimedia-dev qt5-qtserialport-dev qt5-qttools-dev asciidoctor asciidoc alsa-lib-dev linux-headers"
 
 apk add --no-cache $STATIC_PACKAGES
 apk add --no-cache --virtual .build-deps $BUILD_PACKAGES
@@ -84,5 +84,14 @@ WSJT_TGZ=${WSJT_DIR}.tgz
 wget http://physics.princeton.edu/pulsar/k1jt/$WSJT_TGZ
 tar xvfz $WSJT_TGZ
 cmakebuild $WSJT_DIR
+
+git clone https://github.com/wb2osz/direwolf.git
+cd direwolf
+git checkout 1.5
+patch -Np1 < /direwolf-1.5.patch
+make
+make install
+cd ..
+rm -rf direwolf
 
 apk del .build-deps
