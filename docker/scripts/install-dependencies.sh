fdd74e2e096051b9346c116087554b8d79541ecc
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-08-25 16:30:01 +0200

    remove patch (included in git now)

--- docker/scripts/install-dependencies.sh
+++ docker/scripts/install-dependencies.sh
@@ -25,42 +25,6 @@ cmakebuild itpp
 
 git clone https://github.com/jketterl/csdr.git -b 48khz_filter
 cd csdr
-patch -Np1 <<'EOF'
---- a/csdr.c
-+++ b/csdr.c
-@@ -38,6 +38,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- #include <sys/time.h>
- #include <sys/types.h>
- #include <sys/stat.h>
-+#include <sys/select.h>
- #include <fcntl.h>
- #include <sys/ioctl.h>
- #include <unistd.h>
-diff --git a/ddcd_old.h b/ddcd_old.h
-index af4cfb5..b70092b 100644
---- a/ddcd_old.h
-+++ b/ddcd_old.h
-@@ -19,6 +19,7 @@
- #include <stdarg.h>
- #include <sys/stat.h>
- #include <semaphore.h>
-+#include <sys/select.h>
- 
- typedef struct client_s
- {
-diff --git a/nmux.h b/nmux.h
-index 038bc51..079e416 100644
---- a/nmux.h
-+++ b/nmux.h
-@@ -11,6 +11,7 @@
- #include <arpa/inet.h>
- #include <sys/socket.h>
- #include <netinet/in.h>
-+#include <sys/select.h>
- #include "tsmpool.h"
- 
- #define MSG_START "nmux: "
-EOF
 make
 make install
 cd ..
