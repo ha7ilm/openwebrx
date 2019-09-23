e24de8334f19d3d9cba8b59367a825a2ba634f45
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-09-23 16:51:24 +0200

    silence direwolf

--- csdr.py
+++ csdr.py
@@ -240,7 +240,7 @@ class dsp(object):
             chain = secondary_chain_base + "csdr fmdemod_quadri_cf | "
             if self.last_decimation != 1.0:
                 chain += "csdr fractional_decimator_ff {last_decimation} | "
-            chain += "csdr convert_f_s16 | direwolf -c {direwolf_config} -r {audio_rate} -t 0 - 1>&2"
+            chain += "csdr convert_f_s16 | direwolf -c {direwolf_config} -r {audio_rate} -t 0 -q d -q h - 1>&2"
             return chain
 
     def set_secondary_demodulator(self, what):
