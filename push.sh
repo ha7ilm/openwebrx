6bde6236981103be035f9937371be005ebc8aa7c
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-11-18 14:42:05 +0100

    add manifest stuff

--- push.sh
+++ push.sh
@@ -3,6 +3,14 @@ set -euxo pipefail
 
 ARCH=$(uname -m)
 
+ALL_ARCHS="x86_64 armv7l"
+
 for image in openwebrx-rtlsdr openwebrx-sdrplay openwebrx-hackrf openwebrx-airspy openwebrx-full openwebrx; do
   docker push jketterl/$image:$ARCH
+  IMAGE_LIST=""
+  for a in $ALL_ARCHS; do
+    IMAGE_LIST="$IMAGE_LIST jketterl/$image:$a"
+  done
+  docker manifest create --amend jketterl/$image:latest $IMAGE_LIST
+  docker manifest push jketterl/$image:latest
 done
