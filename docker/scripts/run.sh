5a2e8d8f80e71f644589f899c10d44c7a6c1e88b
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-14 19:05:22 +0100

    move config to /etc/openwebrx

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ ! -f /config/config_webrx.py ]] ; then
-  cp config_webrx.py /config
+if [[ ! -f /etc/openwebrx/config_webrx.py ]] ; then
+  mkdir -p /etc/openwebrx/
+  cp config_webrx.py /etc/openwebrx/
 fi
-
-rm config_webrx.py
-ln -s /config/config_webrx.py .
-
-
 _term() {
   echo "Caught signal!" 
   kill -TERM "$child" 2>/dev/null
