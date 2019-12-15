721ac5e2a3c48b26a86f70b704a57af396161c2c
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-15 18:28:35 +0100

    additional files for docker

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
+mkdir -p /etc/openwebrx/
+mkdir -p /tmp/openwebrx/
 if [[ ! -f /etc/openwebrx/config_webrx.py ]] ; then
-  mkdir -p /etc/openwebrx/
-  cp config_webrx.py /etc/openwebrx/
+  sed 's/temporary_directory = "\/tmp"/temporary_directory = "\/tmp\/openwebrx"/' < "/opt/openwebrx/config_webrx.py" > "/etc/openwebrx/config_webrx.py"
 fi
+if [[ ! -f /etc/openwebrx/bands.json ]] ; then
+  cp bands.json /etc/openwebrx/
+fi
+if [[ ! -f /etc/openwebrx/bookmarks.json ]] ; then
+  cp bookmarks.json /etc/openwebrx/
+fi
+
+
 _term() {
   echo "Caught signal!" 
   kill -TERM "$child" 2>/dev/null
