15a2e638661a236b7f4060bd91ad381dceb2f484
Jakob Ketterl <dd5jfk@darc.de>
2019-12-27 11:36:45 +0100

    combine arch and latest

--- build.sh
+++ build.sh
@@ -3,14 +3,13 @@ set -euxo pipefail
 
 ARCH=$(uname -m)
 
-BASE_IMAGE=alpine:3.10
+TAG="latest"
+ARCHTAG="$TAG-$ARCH"
 
-TAGS=$ARCH
-
-docker build --pull --no-cache --build-arg BASE_IMAGE=$BASE_IMAGE -t openwebrx-base:$ARCH -f docker/Dockerfiles/Dockerfile-base .
-docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-rtlsdr:$ARCH -f docker/Dockerfiles/Dockerfile-rtlsdr .
-docker build --build-arg ARCH=$ARCH -t openwebrx-soapysdr-base:$ARCH -f docker/Dockerfiles/Dockerfile-soapysdr .
-docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-sdrplay:$ARCH -f docker/Dockerfiles/Dockerfile-sdrplay .
-docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-hackrf:$ARCH -f docker/Dockerfiles/Dockerfile-hackrf .
-docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-airspy:$ARCH -f docker/Dockerfiles/Dockerfile-airspy .
-docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-full:$ARCH -t jketterl/openwebrx:$ARCH -f docker/Dockerfiles/Dockerfile-full .
+docker build --pull --no-cache -t openwebrx-base:$ARCHTAG -f docker/Dockerfiles/Dockerfile-base .
+docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-rtlsdr:$ARCHTAG -f docker/Dockerfiles/Dockerfile-rtlsdr .
+docker build --build-arg ARCH=$ARCH -t openwebrx-soapysdr-base:$ARCHTAG -f docker/Dockerfiles/Dockerfile-soapysdr .
+docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-sdrplay:$ARCHTAG -f docker/Dockerfiles/Dockerfile-sdrplay .
+docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-hackrf:$ARCHTAG -f docker/Dockerfiles/Dockerfile-hackrf .
+docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-airspy:$ARCHTAG -f docker/Dockerfiles/Dockerfile-airspy .
+docker build --build-arg ARCH=$ARCH -t jketterl/openwebrx-full:$ARCHTAG -t jketterl/openwebrx:$ARCHTAG -f docker/Dockerfiles/Dockerfile-full .
--- push.sh
+++ push.sh
@@ -4,12 +4,14 @@ set -euxo pipefail
 ARCH=$(uname -m)
 
 ALL_ARCHS="x86_64 armv7l aarch64"
+TAG="latest"
+ARCHTAG="$TAG-$ARCH"
 
 for image in openwebrx-rtlsdr openwebrx-sdrplay openwebrx-hackrf openwebrx-airspy openwebrx-full openwebrx; do
-  docker push jketterl/$image:$ARCH
+  docker push jketterl/$image:$ARCHTAG
   IMAGE_LIST=""
   for a in $ALL_ARCHS; do
-    IMAGE_LIST="$IMAGE_LIST jketterl/$image:$a"
+    IMAGE_LIST="$IMAGE_LIST jketterl/$image:$TAG-$a"
   done
   docker manifest create --amend jketterl/$image:latest $IMAGE_LIST
   docker manifest push --purge jketterl/$image:latest
