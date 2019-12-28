71b8d72da373d406d2d958ae9552661e74c1a5aa
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-28 17:17:10 +0100

    push first, ask questions later

--- push.sh
+++ push.sh
@@ -9,10 +9,15 @@ ARCHTAG="$TAG-$ARCH"
 
 for image in openwebrx-rtlsdr openwebrx-sdrplay openwebrx-hackrf openwebrx-airspy openwebrx-full openwebrx; do
   docker push jketterl/$image:$ARCHTAG
+done
+
+for image in openwebrx-rtlsdr openwebrx-sdrplay openwebrx-hackrf openwebrx-airspy openwebrx-full openwebrx; do
+  # there's no docker manifest rm command, and the create --amend does not work, so we have to clean up manually
+  rm -rf "${HOME}/.docker/manifests/docker.io_jketterl_${image}-${TAG}"
   IMAGE_LIST=""
   for a in $ALL_ARCHS; do
     IMAGE_LIST="$IMAGE_LIST jketterl/$image:$TAG-$a"
   done
-  docker manifest create --amend jketterl/$image:latest $IMAGE_LIST
+  docker manifest create jketterl/$image:$TAG $IMAGE_LIST
   docker manifest push --purge jketterl/$image:latest
 done
