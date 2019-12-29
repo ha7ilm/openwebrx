57dab75832c3a17fb74514406fe14bf2c274aa40
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-30 00:12:03 +0100

    re-enable build cache

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 TAG="latest"
 ARCHTAG="$TAG-$ARCH"
 
-docker build --pull --no-cache -t openwebrx-base:$ARCHTAG -f docker/Dockerfiles/Dockerfile-base .
+docker build --pull -t openwebrx-base:$ARCHTAG -f docker/Dockerfiles/Dockerfile-base .
 docker build --build-arg ARCHTAG=$ARCHTAG -t jketterl/openwebrx-rtlsdr:$ARCHTAG -f docker/Dockerfiles/Dockerfile-rtlsdr .
 docker build --build-arg ARCHTAG=$ARCHTAG -t openwebrx-soapysdr-base:$ARCHTAG -f docker/Dockerfiles/Dockerfile-soapysdr .
 docker build --build-arg ARCHTAG=$ARCHTAG -t jketterl/openwebrx-sdrplay:$ARCHTAG -f docker/Dockerfiles/Dockerfile-sdrplay .
