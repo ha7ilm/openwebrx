aad904f1a1345df03cab964103540f89e128c559
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-05 00:19:20 +0100

    add owrs.source to the list of includes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from owrx.version import strictversion
 setup(
     name="OpenWebRX",
     version=str(strictversion),
-    packages=find_namespace_packages(include=["owrx", "csdr", "htdocs"]),
+    packages=find_namespace_packages(include=["owrx", "owrx.source", "csdr", "htdocs"]),
     package_data={"htdocs": [f[len("htdocs/") :] for f in glob("htdocs/**/*", recursive=True)]},
     entry_points={"console_scripts": ["openwebrx=owrx.__main__:main"]},
     # use the github page for now
