976c15d29a102fcd95758d1a93879f8f934bf692
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-10 22:11:57 +0100

    parse address as a numeric field

--- owrx/pocsag.py
+++ owrx/pocsag.py
@@ -5,4 +5,6 @@ class PocsagParser(Parser):
     def parse(self, raw):
         fields = raw.decode("ascii", "replace").rstrip("\n").split(";")
         meta = {v[0]: "".join(v[1:]) for v in map(lambda x: x.split(":"), fields) if v[0] != ""}
+        if "address" in meta:
+            meta["address"] = int(meta["address"])
         self.handler.write_pocsag_data(meta)
