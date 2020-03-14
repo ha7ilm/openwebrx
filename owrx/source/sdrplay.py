fea2cd1cc5c00ac2fb7d4726cb72f51235e586ee
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-03-14 01:21:43 +0100

    add new settings mappings for rf_notch and dab_notch

--- owrx/source/sdrplay.py
+++ owrx/source/sdrplay.py
@@ -4,7 +4,13 @@ from .soapy import SoapyConnectorSource
 class SdrplaySource(SoapyConnectorSource):
     def getSoapySettingsMappings(self):
         mappings = super().getSoapySettingsMappings()
-        mappings.update({"bias_tee": "biasT_ctrl"})
+        mappings.update(
+            {
+                "bias_tee": "biasT_ctrl",
+                "rf_notch": "rfnotch_ctrl",
+                "dab_notch": "dabnotch_ctrl",
+            }
+        )
         return mappings
 
     def getDriver(self):
