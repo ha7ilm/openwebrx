e37e2f4540711fdb6f4a77823f534a80d3a910bc
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-03-14 01:15:25 +0100

    add biastee setting for sdrplay, too

--- owrx/source/sdrplay.py
+++ owrx/source/sdrplay.py
@@ -2,5 +2,10 @@ from .soapy import SoapyConnectorSource
 
 
 class SdrplaySource(SoapyConnectorSource):
+    def getSoapySettingsMappings(self):
+        mappings = super().getSoapySettingsMappings()
+        mappings.update({"bias_tee": "biasT_ctrl"})
+        return mappings
+
     def getDriver(self):
         return "sdrplay"
