05485ba8e3ff8668c3521ac6f8a27b0d9668550f
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-03-21 18:58:03 +0100

    add perseus basic build (not enabled for now)

---
+++ docker/scripts/install-dependencies-perseus.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+export MAKEFLAGS="-j4"
+
+cd /tmp
+
+STATIC_PACKAGES="libusb udev"
+BUILD_PACKAGES="git make gcc autoconf automake libtool musl-dev libusb-dev shadow"
+
+apk add --no-cache $STATIC_PACKAGES
+apk add --no-cache --virtual .build-deps $BUILD_PACKAGES
+
+git clone https://github.com/Microtelecom/libperseus-sdr.git
+cd libperseus-sdr
+git checkout 72ac67c5b7936a1991be0ec97c03a59c1a8ac8f3
+./bootstrap.sh
+./configure
+make
+make install
+ldconfig /etc/ld.so.conf.d
+cd ..
+rm -rf libperseus-sdr
+
+apk del .build-deps
