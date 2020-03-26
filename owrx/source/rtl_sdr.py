16d5db00afa7c222e10a2e5b8ce2296c4d1a13b8
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-03-26 13:33:32 +0100

    send bias tee configs for rtl_sdr

--- owrx/source/rtl_sdr.py
+++ owrx/source/rtl_sdr.py
@@ -1,6 +1,14 @@
 from .connector import ConnectorSource
+from owrx.command import Flag
 
 
 class RtlSdrSource(ConnectorSource):
     def getCommandMapper(self):
-        return super().getCommandMapper().setBase("rtl_connector")
+        return super().getCommandMapper().setBase("rtl_connector").setMappings(
+            {
+                "bias_tee": Flag("-b")
+            }
+        )
+
+    def getEventNames(self):
+        return super().getEventNames() + ["bias_tee"]
