3f742c7b1a6bb106487cec33e519d0e292dc4789
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-04-01 21:39:53 +0200

    webadmin feature flag

--- owrx/controllers/admin.py
+++ owrx/controllers/admin.py
@@ -1,5 +1,6 @@
 from .template import WebpageController
 from .session import SessionStorage
+from owrx.config import Config
 
 
 class Authentication(object):
@@ -16,6 +17,10 @@ class AdminController(WebpageController):
         super().__init__(handler, request, options)
 
     def handle_request(self):
+        config = Config.get()
+        if not config["webadmin_enabled"]:
+            self.send_response("Web Admin is disabled", code=403)
+            return
         if self.authentication.isAuthenticated(self.request):
             super().handle_request()
         else:
