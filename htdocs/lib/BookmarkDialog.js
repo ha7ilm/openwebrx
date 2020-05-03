e2cacc1fa0c9666d0d22af7b05f1f1ef8ca9b530
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-05-03 23:58:12 +0200

    only available ones

--- htdocs/lib/BookmarkDialog.js
+++ htdocs/lib/BookmarkDialog.js
@@ -2,7 +2,9 @@ $.fn.bookmarkDialog = function() {
     var $el = this;
     return {
         setModes: function(modes) {
-            $el.find('#modulation').html(modes.map(function(m) {
+            $el.find('#modulation').html(modes.filter(function(m){
+                return m.isAvailable();
+            }).map(function(m) {
                 return '<option value="' + m.modulation + '">' + m.name + '</option>';
             }).join(''));
         }
