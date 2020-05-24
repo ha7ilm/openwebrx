048210d7da746b07b088a908461b95b012ff419a
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-05-24 14:59:11 +0200

    update to latest versions from the homepage

--- docker/scripts/install-dependencies-sdrplay.sh
+++ docker/scripts/install-dependencies-sdrplay.sh
@@ -28,13 +28,13 @@ ARCH=$(uname -m)
 
 case $ARCH in
   x86_64)
-    BINARY=SDRplay_RSP_API-Linux-3.06.1.run
+    BINARY=SDRplay_RSP_API-Linux-3.07.1.run
     ;;
   armv*)
-    BINARY=SDRplay_RSP_API-ARM32-3.06.1.run
+    BINARY=SDRplay_RSP_API-ARM32-3.07.2.run
     ;;
   aarch64)
-    BINARY=SDRplay_RSP_API-ARM64-3.06.1.run
+    BINARY=SDRplay_RSP_API-ARM64-3.07.1.run
     ;;
 esac
 
