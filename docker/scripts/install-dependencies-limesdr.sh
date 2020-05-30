1359da5b14a55580364511a7cf9f6197ef466a8d
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-05-31 01:04:57 +0200

    limit SIMD flags to x86 only

--- docker/scripts/install-dependencies-limesdr.sh
+++ docker/scripts/install-dependencies-limesdr.sh
@@ -10,12 +10,17 @@ BUILD_PACKAGES="git libusb-1.0-0-dev cmake make gcc g++"
 apt-get update
 apt-get -y install --no-install-recommends $STATIC_PACKAGES $BUILD_PACKAGES
 
+SIMD_FLAGS=""
+if [[ 'x86_64' == `uname -m` ]] ; then
+    SIMD_FLAGS="-DDEFAULT_SIMD_FLAGS=SSE3"
+fi
+
 git clone https://github.com/myriadrf/LimeSuite.git
 cd LimeSuite
 git checkout 0854a51ec06b30b01f19a562149c39461e92f24d
 mkdir builddir
 cd builddir
-cmake .. -DENABLE_EXAMPLES=OFF -DENABLE_DESKTOP=OFF -DENABLE_LIME_UTIL=OFF -DENABLE_QUICKTEST=OFF -DENABLE_OCTAVE=OFF -DENABLE_GUI=OFF -DCMAKE_CXX_STANDARD_LIBRARIES="-latomic" -DDEFAULT_SIMD_FLAGS=SSE3
+cmake .. -DENABLE_EXAMPLES=OFF -DENABLE_DESKTOP=OFF -DENABLE_LIME_UTIL=OFF -DENABLE_QUICKTEST=OFF -DENABLE_OCTAVE=OFF -DENABLE_GUI=OFF -DCMAKE_CXX_STANDARD_LIBRARIES="-latomic" ${SIMD_FLAGS}
 make
 make install
 cd ../..
