e4cf95856e83cee23a436214ee23c13a98ccecfc
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-11-28 22:11:12 +0100

    update connectors

--- docker/scripts/install-connectors.sh
+++ docker/scripts/install-connectors.sh
@@ -24,7 +24,8 @@ apt-get update
 apt-get -y install --no-install-recommends $BUILD_PACKAGES
 
 git clone https://github.com/jketterl/owrx_connector.git
-cmakebuild owrx_connector 0.3.0
+# latest develop as of 2020-11-28 (c++ refactoring)
+cmakebuild owrx_connector 8c7982f1f91bb38f359126725de64a5875221898
 
 apt-get -y purge --autoremove $BUILD_PACKAGES
 apt-get clean
