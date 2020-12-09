9d6099b6d8f3c5d50818255ce5eb804399c93248
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-12-09 17:38:37 +0100

    FST4[W] frontend work

--- htdocs/openwebrx.js
+++ htdocs/openwebrx.js
@@ -953,7 +953,7 @@ function update_wsjt_panel(msg) {
     };
     var linkedmsg = msg['msg'];
     var matches;
-    if (['FT8', 'JT65', 'JT9', 'FT4'].indexOf(msg['mode']) >= 0) {
+    if (['FT8', 'JT65', 'JT9', 'FT4', 'FST4', 'FST4W'].indexOf(msg['mode']) >= 0) {
         matches = linkedmsg.match(/(.*\s[A-Z0-9]+\s)([A-R]{2}[0-9]{2})$/);
         if (matches && matches[2] !== 'RR73') {
             linkedmsg = html_escape(matches[1]) + '<a href="map?locator=' + matches[2] + '" target="openwebrx-map">' + matches[2] + '</a>';
