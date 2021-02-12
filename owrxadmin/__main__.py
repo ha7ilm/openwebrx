dd2fda54d18161a58bd98ecbf4742c47eaeaeaba
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-02-12 17:00:51 +0100

    add logging setup for owrxadmin

--- owrxadmin/__main__.py
+++ owrxadmin/__main__.py
@@ -3,6 +3,9 @@ from owrxadmin.commands import NewUser, DeleteUser, ResetPassword, ListUsers, Di
 import argparse
 import sys
 import traceback
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 
 def main():
