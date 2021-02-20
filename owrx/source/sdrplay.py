8b24eff72ed5e7ec1067272f08b3da7f0de88aec
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-02-20 19:00:28 +0100

    add sdrplay specific options

--- owrx/source/sdrplay.py
+++ owrx/source/sdrplay.py
@@ -1,6 +1,7 @@
 from owrx.source.soapy import SoapyConnectorSource, SoapyConnectorDeviceDescription
-from owrx.form import Input
+from owrx.form import Input, CheckboxInput, DropdownInput, DropdownEnum
 from owrx.form.device import BiasTeeInput
+from owrx.form.converter import OptionalConverter, EnumConverter
 from typing import List
 
 
@@ -22,9 +23,44 @@ class SdrplaySource(SoapyConnectorSource):
         return "sdrplay"
 
 
+class IfModeOptions(DropdownEnum):
+    IFMODE_ZERO_IF = "Zero-IF"
+    IFMODE_450 = "450kHz"
+    IFMODE_1620 = "1620kHz"
+    IFMODE_2048 = "2048kHz"
+
+    def __str__(self):
+        return self.value
+
+
 class SdrplayDeviceDescription(SoapyConnectorDeviceDescription):
     def getGainStages(self):
         return ["RFGR", "IFGR"]
 
     def getInputs(self) -> List[Input]:
-        return self.mergeInputs(super().getInputs(), [BiasTeeInput()])
+        return self.mergeInputs(
+            super().getInputs(),
+            [
+                BiasTeeInput(),
+                CheckboxInput(
+                    "rf_notch",
+                    "",
+                    checkboxText="Enable RF notch filter",
+                    converter=OptionalConverter(defaultFormValue=True),
+                ),
+                CheckboxInput(
+                    "dab_notch",
+                    "",
+                    checkboxText="Enable DAB notch filter",
+                    converter=OptionalConverter(defaultFormValue=True),
+                ),
+                DropdownInput(
+                    "if_mode",
+                    "IF Mode",
+                    IfModeOptions,
+                    converter=OptionalConverter(
+                        EnumConverter(IfModeOptions), defaultFormValue=IfModeOptions.IFMODE_ZERO_IF.name
+                    ),
+                ),
+            ],
+        )
