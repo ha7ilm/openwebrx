25d04f4cbc8e469e9c0f4cd06ad1cf6d07fcd6ab
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-04-26 21:27:15 +0200

    exclude keys that have been moved to openwebrx.conf

--- owrx/config/commands.py
+++ owrx/config/commands.py
@@ -4,13 +4,21 @@ from owrx.bookmarks import Bookmarks
 
 
 class MigrateCommand(Command):
+    # these keys have been moved to openwebrx.conf
+    blacklisted_keys = [
+        "temporary_directory",
+        "web_port",
+        "aprs_symbols_path",
+    ]
+
     def run(self, args):
         print("Migrating configuration...")
 
         config = Config.get()
         # a key that is set will end up in the DynamicConfig, so this will transfer everything there
         for key, value in config.items():
-            config[key] = value
+            if key not in MigrateCommand.blacklisted_keys:
+                config[key] = value
         config.store()
 
         print("Migrating bookmarks...")
