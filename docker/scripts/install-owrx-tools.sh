ddcdd550fdfbb361588b137842bb6635970ccb0e
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-06-17 15:01:10 +0200

    update dependencies

--- docker/scripts/install-owrx-tools.sh
+++ docker/scripts/install-owrx-tools.sh
@@ -43,12 +43,12 @@ rm -rf csdr
 git clone https://github.com/jketterl/codecserver.git
 mkdir -p /usr/local/etc/codecserver
 cp codecserver/conf/codecserver.conf /usr/local/etc/codecserver
-#latest develop as of 2021-06-15 (queue fixes)
-cmakebuild codecserver 2cc0cac622304ea124daac1397ff8e17154f1f2b
+#latest develop as of 2021-06-17 (docker image)
+cmakebuild codecserver 7dc6084b2376944df238efba4ed48473750f67d3
 
 git clone https://github.com/jketterl/digiham.git
-#latest develop as of 2021-06-15 (dstar and nxdn addition)
-cmakebuild digiham 2d426c06cf3c7f25a6c097a19a4f01c5907e0e3d
+#latest develop as of 2021-06-15 (DMR LCSS overflow fix; D-Star alternate terminator)
+cmakebuild digiham 418145d74b528596a39198a537ab56207d932595
 
 apt-get -y purge --autoremove $BUILD_PACKAGES
 apt-get clean
