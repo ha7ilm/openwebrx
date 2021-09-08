ca0f7af1d0f9ce044352c31beb4d313c6ccc0096
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-09-08 13:48:11 +0200

    use stereo downmix; disable squelch for DRM

--- csdr/chain/drm.py
+++ csdr/chain/drm.py
@@ -1,13 +1,16 @@
 from csdr.chain.demodulator import BaseDemodulatorChain, FixedIfSampleRateChain, FixedAudioRateChain
-from pycsdr.modules import Convert
+from pycsdr.modules import Convert, Downmix
 from pycsdr.types import Format
 from owrx.drm import DrmModule
 
 
 class Drm(BaseDemodulatorChain, FixedIfSampleRateChain, FixedAudioRateChain):
     def __init__(self):
-        workers = [Convert(Format.COMPLEX_FLOAT, Format.COMPLEX_SHORT), DrmModule()]
+        workers = [Convert(Format.COMPLEX_FLOAT, Format.COMPLEX_SHORT), DrmModule(), Downmix()]
         super().__init__(workers)
+
+    def supportsSquelch(self) -> bool:
+        return False
 
     def getFixedIfSampleRate(self) -> int:
         return 48000
