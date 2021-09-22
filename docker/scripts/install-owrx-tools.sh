981948b708b3568b4d18dcf437ce40037fa5d86f
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-09-22 18:15:47 +0200

    update components in docker build

--- docker/scripts/install-owrx-tools.sh
+++ docker/scripts/install-owrx-tools.sh
@@ -18,8 +18,8 @@ function cmakebuild() {
 
 cd /tmp
 
-STATIC_PACKAGES="libfftw3-bin libprotobuf17"
-BUILD_PACKAGES="git autoconf automake libtool libfftw3-dev pkg-config cmake make gcc g++ libprotobuf-dev protobuf-compiler"
+STATIC_PACKAGES="libfftw3-bin libprotobuf17 libsamplerate0 libicu63"
+BUILD_PACKAGES="git autoconf automake libtool libfftw3-dev pkg-config cmake make gcc g++ libprotobuf-dev protobuf-compiler libsamplerate-dev libicu-dev libpython3-dev"
 apt-get update
 apt-get -y install --no-install-recommends $STATIC_PACKAGES $BUILD_PACKAGES
 
@@ -31,14 +31,16 @@ popd
 rm -rf js8py
 
 git clone https://github.com/jketterl/csdr.git
-cd csdr
-git checkout 0.17.0
-autoreconf -i
-./configure
-make
-make install
+# latest develop as of 2021-09-22 (template fixes)
+cmakebuild csdr 536f3b9eb7cfe5434e9a9f1e807c96115dc9ac10
+
+git clone https://github.com/jketterl/pycsdr.git
+cd pycsdr
+# latest develop as of 2021-09-22 (first version)
+git checkout 52da48a87ef97eb7d337f1b146db66ca453801e4
+./setup.py install install_headers
 cd ..
-rm -rf csdr
+rm -rf pycsdr
 
 git clone https://github.com/jketterl/codecserver.git
 mkdir -p /usr/local/etc/codecserver
@@ -46,7 +48,16 @@ cp codecserver/conf/codecserver.conf /usr/local/etc/codecserver
 cmakebuild codecserver 0.1.0
 
 git clone https://github.com/jketterl/digiham.git
-cmakebuild digiham 0.5.0
+# latest develop as of 2021-09-22 (post-merge)
+cmakebuild digiham 62d2b4581025568263ae8c90d2450b65561b7ce8
+
+git clone https://github.com/jketterl/pydigiham.git
+cd pydigiham
+# latest develop as of 2021-09-22 (split from digiham)
+git checkout b0cc0c35d5ef2ae84c9bb1a02d56161d5bd5bf2f
+./setup.py install
+cd ..
+rm -rf pydigiham
 
 apt-get -y purge --autoremove $BUILD_PACKAGES
 apt-get clean
