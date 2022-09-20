6c01d484934e3204ab4610b35ef8c83acfbdb863
Jakob Ketterl <jakob.ketterl@gmx.de>
2022-09-20 18:06:03 +0200

    update version

--- owrx/version.py
+++ owrx/version.py
@@ -1,5 +1,5 @@
 from distutils.version import LooseVersion
 
-_versionstring = "1.2.0"
+_versionstring = "1.2.1"
 looseversion = LooseVersion(_versionstring)
 openwebrx_version = "v{0}".format(looseversion)
